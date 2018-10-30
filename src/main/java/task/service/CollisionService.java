84dd700979b748eb2acb3068ab70e5976d852c1b
Roman <roman.poshtak@privatbank.ua>
2018-10-30 14:26:35 +0200

    completed

--- src/main/java/task/service/CollisionService.java
+++ src/main/java/task/service/CollisionService.java
@@ -1,7 +1,51 @@
 package task.service;
 
 import org.springframework.stereotype.Service;
+import task.domains.Response;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CollisionService {
+
+    /**
+     * method is looking for collisions for list of word
+     * @param wordList - list of word
+     * @return response with collisions
+     */
+    public Response searchCollision(List<String> wordList) {
+        Response response = new Response();
+        response.setOutList(new ArrayList<>());
+        Iterator<String> iterator = wordList.iterator();
+        while (iterator.hasNext()) {
+            String word = iterator.next();
+            List<String> collisionWords = findWordCollisions(word, wordList);
+            if (!collisionWords.isEmpty() && collisionWords.size() > 1) {
+                response.getOutList().add(collisionWords);
+                wordList.removeAll(collisionWords);
+                iterator = wordList.iterator();
+            }
+        }
+        return response;
+    }
+
+
+    /**
+     * method is looking for collisions for one word
+     * @param inWord - the word for search
+     * @param words - list of words
+     * @return list of collisions
+     */
+    private static List<String> findWordCollisions(String inWord, List<String> words) {
+        // get lower-cased letters
+        String lowerWord = inWord.toLowerCase();
+        int length = inWord.length();
+        return words.stream()
+                .filter(s -> s.length() == length)
+                .filter(str -> lowerWord.chars().sum() == str.toLowerCase().chars().sum())
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/task/web/CollisionController.java
+++ src/main/java/task/web/CollisionController.java
@@ -1,5 +1,6 @@
 package task.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,8 +9,7 @@ import task.domains.Request;
 import task.domains.Response;
 import task.service.CollisionService;
 
-import java.util.Arrays;
-import java.util.List;
+import java.io.IOException;
 
 @RestController
 public class CollisionController {
@@ -21,8 +21,18 @@ public class CollisionController {
         this.collisionService = collisionService;
     }
 
-    @PostMapping("getCollisions")
-    public Response searchCollision(@RequestBody Request words) {
-        return null;
+    /**
+     * accepts an array of strings and returns the sets of strings which collide in the hash table
+     * @param requestBody - request from client
+     * @return - list with collisions
+     */
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public Response searchCollision(@RequestBody String requestBody) {
+        try {
+            Request request = new ObjectMapper().readValue(requestBody, Request.class);
+            return collisionService.searchCollision(request.getInList());
+        } catch (IOException e) {
+            throw new IllegalStateException("Please, enter correct data in json format");
+        }
     }
 }
